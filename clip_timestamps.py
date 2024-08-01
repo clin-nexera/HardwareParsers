0726ed3a2d3b1b7bcd56493f68dfd44ce17ba006
clin <candy.lin@nexera-robotics.com>
2024-08-01 12:16:07 -0700

    video clipping

---
+++ clip_timestamps.py
@@ -0,0 +1,64 @@
+import os
+from datetime import datetime, timedelta
+from tkinter.filedialog import askopenfilename, askdirectory
+
+from moviepy.editor import *
+
+VIDEO_CLIP_TIME_S = 10
+
+if __name__ == "__main__":
+    video_path = askopenfilename(title="Select Video")
+    video_dir = os.path.dirname(video_path)
+
+    if not video_path or video_path == "":
+        raise Exception("Invalid video path")
+    
+    timestamps_path = askopenfilename(title="Select Timestamps")
+
+    if not timestamps_path or timestamps_path == "":
+        raise Exception("Invalid timestamps path")
+    
+    save_dir = askdirectory(title="Select Save Folder")
+
+    if not save_dir or save_dir == "":
+        raise Exception("Invalid save folder")
+
+    clip = VideoFileClip(video_path, verbose=False)
+    video_filename = os.path.basename(video_path)
+    clip_start_time = datetime.strptime(video_filename, "%Y-%m-%d %H-%M-%S.mkv")
+    clip_end_time = clip_start_time + timedelta(seconds=clip.duration)
+
+    with open(timestamps_path, "r") as f:
+        timestamps_text = f.read()
+
+    timestamps = timestamps_text.split("\n")
+    
+    for timestamp in timestamps:
+        if timestamp.strip() == "":
+            continue
+
+        event_time = datetime.strptime(timestamp, "%Y\\%m\\%d %H:%M:%S")
+        event_time_str = event_time.strftime("%Y-%m-%d_%H-%M_%S")
+
+        if event_time < clip_start_time or event_time > clip_end_time:
+            print(f"!!! Timestamp Not Within This Clip: {timestamp} | {event_time_str} !!!")
+            continue
+        
+        event_time_start = event_time - timedelta(seconds = int(VIDEO_CLIP_TIME_S/2))
+        event_time_end = event_time + timedelta(seconds = int(VIDEO_CLIP_TIME_S/2))
+
+        if event_time_start < clip_start_time:
+            event_time_start = clip_start_time
+
+        if event_time_end > clip_end_time:
+            event_time_end = clip_end_time
+            
+        time_elapsed_start = int((event_time_start-clip_start_time).total_seconds())
+        time_elapsed_end =int((event_time_end-clip_start_time).total_seconds())
+
+        sub_clip: VideoFileClip = clip.subclip(time_elapsed_start, time_elapsed_end)
+        sub_clip.write_videofile(os.path.join(save_dir, f"{event_time_str}.mp4"), logger=None)
+
+
+
+
--- readme.md
+++ readme.md
@@ -12,3 +12,17 @@
 2) The terminal will prompt you for a file name: E.g. bottles
 3) A window will pop up to let you choose the saving location of your output csv file
 4) A window will pop up to let you select multiple folders
+
+# Clipping Videos
+## Dependencies
+Install moviepy if it hasn't already been: ```pip install moviepy```
+
+
+## How To Run
+1) In terminal type: ```py clip_timestamps.py```
+2) Select video
+3) Select the timestamp text file
+4) Select the folder to save clips in
+
+## Notes
+You can adjust total clip times inside clip_timestamps.py with the VIDEO_CLIP_TIME_S variable
